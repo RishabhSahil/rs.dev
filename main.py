377a5f951329c5668ca358d12199ef7e46ba665d
--v--: ᏒᎥShᎪbH :--v-- <91429171+RishabhSahil@users.noreply.github.com>
2023-04-20 20:07:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,62 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, redirect, request, render_template
+import search as sr
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route('/', methods =["GET", "POST"])
+def home():
+    return render_template('index.html')
 
+
+@app.route(f'/search', methods =["GET", "POST"])
+def search():
+    search = request.args.get("q")
+    aa=sr.search_query(search.lower())
+    answer = aa
+    if search==False:
+        return render_template('index.html')
+    if request.method == 'GET': 
+        request.form.get('q')
+        data={
+            "title":search,
+            "search":search,
+            "answer":answer,
+            "idlinks":list(answer["links"].keys()) 
+        }
+        print(str(search))
+    return render_template('search.html',data=data)
+
+@app.route('/translator', methods =["GET", "POST"])
+def translatorr():
+    lang = sr.languages()
+    data = {
+        "lang": lang,
+        "langv": list(lang.keys()),
+        "langse": "en",
+        "langvles": "english",
+        "answer": "Translation",
+        "title": "Language Translator"
+    } 
+    if request.method == 'POST':
+        query = request.form.get('question')
+        select = request.form.get('comp_select')
+        langvle = lang[select]
+        query = str(query)
+        trans_result=sr.rishabh_translate(text=query,lang=select)
+        langq = trans_result.src
+        data = {
+            "lang": lang,
+            "langv": list(lang.keys()),
+            "langse": select,
+            "langvles": langvle,
+            "langqu": langq.lower(),
+            "question": query,
+            "answer": trans_result,
+            "title": query
+        }
+    return render_template("translator.html",data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+   app.run(host="0.0.0.0", port=5000, debug=True)
+    # app.run()
